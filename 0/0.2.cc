90056d49926cc9a39ef28a436ab56f32bbb00a57
Jesper Wallengren <jesper.wallengren@gmail.com>
2025-07-30 23:15:12 +0200

    Partway done with 0.2

--- 0/0.2.cc
+++ 0/0.2.cc
@@ -7,68 +7,70 @@
 using namespace std;
 
 const vector<string> split(const string& input) {
-    string product;
-    char del(' ');
-    vector<string> output;
+string product;
+	char del(' ');
+	vector<string> output;
 
-    for (char c : input) {
-        if(c == del) {
-            output.push_back(product);
-            product = "";
-        }
+	for (char c : input) {
+			if(c == del) {
+					output.push_back(product);
+					product = "";
+			}
 
-        product.push_back(c);
-    }
+			product.push_back(c);
+	}
 
-    return output;
+	return output;
 }
 
 int main() {
-    string target_file = "UPPloppet_resultat.txt";
+	// Default file and rows
+	string target_file = "UPPloppet_resultat.txt";
+	int rows = 5;
 
-    // string target_file{""};
-    // cout << "Skriv namnet av en fil: ";
-    // getline(cin, target_file);
+	// cout << "Antal rader: " << target_file << endl;
+	// getline(cin, target_file);
+	// cout << "Antal rader: ";
+	// cin >> rows;
 
-    ifstream file(target_file);
+	ifstream file(target_file);
 
-    if (!file.is_open()) {
-        cerr << "Failed to open file\n" << endl;
-        return 1;
-    }
+	if (!file.is_open()) {
+		cerr << "Failed to open file\n" << endl;
+		return 1;
+	}
 
-    // String to store each line in
-    struct Racer {
-        string name;
-        int hours;
-        int minutes;
-        int seconds;
-    };
+	struct Racer {
+			string name;
+			int hours;
+			int minutes;
+			int seconds;
+	};
 
-    string line;
-    vector<Racer> data;
+	string line;
+	vector<Racer> data;
 
-    while (getline(file, line)) {
-        Racer racer;
-        vector<string> tokens = split(line);
-        racer.name = tokens[0];
-        racer.hours = stoi(tokens[1]);
-        racer.minutes = stoi(tokens[2]);
-        racer.seconds = stoi(tokens[3]);
-        data.push_back(racer);
-    }
+	while (getline(file, line)) {
+			Racer racer;
+			vector<string> tokens = split(line);
+			racer.name = tokens[0];
+			racer.hours = stoi(tokens[1]);
+			racer.minutes = stoi(tokens[2]);
+			racer.seconds = stoi(tokens[3]);
+			data.push_back(racer);
+	}
+	file.close();
 
-    sort(data.begin(), data.end(), 
-         [](Racer a, Racer b){
-            int a_total = a.hours * 60 * 60 + a.minutes * 60 + a.seconds;
-            int b_total = b.hours * 60 * 60 + b.minutes * 60 + b.seconds;
-            return a_total > b_total;
-         });
+	sort(data.begin(), data.end(), 
+		[](Racer a, Racer b){
+			int a_total = a.hours * 60 * 60 + a.minutes * 60 + a.seconds;
+			int b_total = b.hours * 60 * 60 + b.minutes * 60 + b.seconds;
+			return a_total > b_total;
+		});
 
-    for (Racer racer : data) {
-        int total = racer.hours * 60 * 60 + racer.minutes * 60 + racer.seconds;
-        cout << total << endl;
-    }
+	for (Racer racer : data) {
+			int total = racer.hours * 60 * 60 + racer.minutes * 60 + racer.seconds;
+			cout << total << endl;
+	}
 
-    file.close();
 }
