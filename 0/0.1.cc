a84d7abb03cab0a03565409bcc27fe7512c3962b
Jesper Wallengren <jesper.wallengren@gmail.com>
2025-08-03 20:43:08 +0200

    Small changes

--- 0/0.1.cc
+++ 0/0.1.cc
@@ -1,74 +1,89 @@
+#include <iomanip>
 #include <iostream>
+#include <sstream>
 #include <string>
 using namespace std;
 
 int main() {
-    int i;
-    float f;
-    char c;
-    string s;
+  int i;
+  float f;
+  string s;
 
-    cout << "Skriv in ett heltal: " << flush;
-    cin >> i;
-    while (cin.get() != '\n');
-    cout << "Du skrev in talet: " << i << "\n" << endl;
+  cout << "Skriv in ett heltal: " << flush;
+  cin >> i;
+  while (cin.get() != '\n')
+    ;
+  cout << "Du skrev in talet: " << i << "\n" << endl;
 
-    cout << "Skriv in fem decimaltal: " << flush;
-    for (int j = 0; j < 5; ++j) {
-        cin >> f;
-        cout << f << " ";
-    }
-    cout << "\n";
-    while (cin.get() != '\n');
+  cout << "Skriv in fem decimaltal: " << flush;
+  getline(cin, s);
+  cout << "Du skrev in talen: ";
+  istringstream iss(s);
+  for (i = 0; i < 5 && iss >> f; ++i) {
+    cout << f << " ";
+  }
+  cout << "\n\n" << flush;
 
-    cout << "Skriv in ett heltal och ett flyttal: " << flush;
-    cin >> i >> f;
-    while (cin.get() != '\n');
-    cout << "Du skrev in heltalet: " << i << "\n";
-    cout << "Du skrev in flyttalet: " << f << "\n";
+  cout << "Skriv in ett heltal och ett flyttal: " << flush;
+  cin >> i >> f;
+  while (cin.get() != '\n')
+    ;
+  cout << "\n" << flush;
 
-    cout << "Skriv in ett flyttal och ett heltal:  " << flush;
-    cin >> f >> i;
-    while (cin.get() != '\n');
-    cout << "Du skrev in heltalet: " << i << "\n";
-    cout << "Du skrev in flyttalet: " << f << "\n";
+  cout << setprecision(4);
+  cout << setw(15) << setfill('-') << left
+       << "Du skrev in heltalet:" << setw(11) << right << i << "\n";
+  cout << setw(15) << left << "Du skrev in flyttalet:" << setw(10) << right << f
+       << "\n\n";
 
-    cout << "Skriv in ett tecken:  " << flush;
-    cin >> c;
-    while (cin.get() != '\n');
-    cout << "Du skrev in tecknet: " << c << "\n";
+  cout << "Skriv in ett flyttal och ett heltal: " << flush;
+  cin >> f >> i;
+  while (cin.get() != '\n')
+    ;
+  cout << "Du skrev in heltalet: " << i << "\n";
+  cout << "Du skrev in flyttalet: " << f << "\n";
 
-    cout << "Skriv in ett ord: " << flush;
-    cin >> s;
-    while (cin.get() != '\n');
-    cout << "Du skrev in ordet: " << s << "\n";
+  cout << "Skriv in ett tecken:  " << flush;
+  // cin >> c;
+  while (cin.get() != '\n')
+    ;
+  // cout << "Du skrev in tecknet: " << c << "\n";
 
-    cout << "Skriv in ett heltal och ett ord: " << flush;
-    cin >> i >> s;
-    while (cin.get() != '\n');
-    cout << "Du skrev in talet " << i << " och ordet " << s << "\n";
+  cout << "Skriv in ett ord: " << flush;
+  cin >> s;
+  while (cin.get() != '\n')
+    ;
+  cout << "Du skrev in ordet: " << s << "\n";
 
-    cout << "Skriv in ett tecken och ett ord:  " << flush;
-    cin >> c >> s;
-    while (cin.get() != '\n');
-    cout << "Du skrev in " << c << " och " << s << "\n";
+  cout << "Skriv in ett heltal och ett ord: " << flush;
+  cin >> i >> s;
+  while (cin.get() != '\n')
+    ;
+  cout << "Du skrev in talet " << i << " och ordet " << s << "\n";
 
-    cout << "Skrev in en rad text:  " << flush;
-    getline(cin, s);
-    cout << "Du skrev in: " << s << "\n";
+  cout << "Skriv in ett tecken och ett ord:  " << flush;
+  // cin >> c >> s;
+  while (cin.get() != '\n')
+    ;
+  // cout << "Du skrev in " << c << " och " << s << "\n";
 
-    cout << "Skrev in en till rad text:  " << flush;
-    getline(cin, s);
-    cout << "Du skrev in: " << s << "\n";
+  cout << "Skrev in en rad text:  " << flush;
+  getline(cin, s);
+  cout << "Du skrev in: " << s << "\n";
 
-    cout << "Skriv in tre ord:  " << flush;
-    cin >> s;
-    cout << s << " ";
-    cin >> s;
-    cout << s << " ";
-    cin >> s;
-    cout << s << "\n";
-    while (cin.get() != '\n');
+  cout << "Skrev in en till rad text:  " << flush;
+  getline(cin, s);
+  cout << "Du skrev in: " << s << "\n";
 
-    return 0;
+  cout << "Skriv in tre ord:  " << flush;
+  cin >> s;
+  cout << s << " ";
+  cin >> s;
+  cout << s << " ";
+  cin >> s;
+  cout << s << "\n";
+  while (cin.get() != '\n')
+    ;
+
+  return 0;
 }
--- 0/0.2.cc
+++ 0/0.2.cc
@@ -1,113 +1,112 @@
+#include <algorithm>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <limits>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 struct Racer {
-	string name;
-	int hours;
-	int minutes;
-	int seconds;
+  string name;
+  int hours;
+  int minutes;
+  int seconds;
 
-	Racer() : name(""), hours(0), minutes(0), seconds(0) {}
+  Racer() : name(""), hours(0), minutes(0), seconds(0) {}
 };
 
-const vector<string> split(const string& input) {
-	string product;
-	char del(' ');
-	vector<string> output;
+const vector<string> split(const string &input) {
+  string product;
+  char del(' ');
+  vector<string> output;
 
-	for (char c : input) {
-		if(c == del) {
-			output.push_back(product);
-			product = "";
-		}
+  for (char c : input) {
+    if (c == del) {
+      output.push_back(product);
+      product = "";
+    }
 
-			product.push_back(c);
-	}
+    product.push_back(c);
+  }
 
-	return output;
+  return output;
 }
 
-void printCentered(const std::string& text, int width) {
-    int total_pad = width - text.size();
-    int left_pad = total_pad / 2;
-    int right_pad = total_pad - left_pad;
+void printCentered(const std::string &text, int width) {
+  int total_pad = width - text.size();
+  int left_pad = total_pad / 2;
+  int right_pad = total_pad - left_pad;
 
-    if (total_pad > 0)
-        std::cout << std::string(left_pad, ' ') << text << std::string(right_pad, ' ');
-    else
-        std::cout << text;
+  if (total_pad > 0)
+    std::cout << std::string(left_pad, ' ') << text
+              << std::string(right_pad, ' ');
+  else
+    std::cout << text;
 }
 
 int main() {
 file:
-	string target_file = "UPPloppet_resultat.txt";
+  string target_file = "UPPloppet_resultat.txt";
 
-	cout << "Ange filnamn: ";
-	getline(cin, target_file);
+  cout << "Ange filnamn: ";
+  getline(cin, target_file);
 
-	ifstream file(target_file);
+  ifstream file(target_file);
 
-	if (!file.is_open()) {
-		cerr << "FEL: Filen gick inte att öppna!" << endl;
-		goto file;
-	}
+  if (!file.is_open()) {
+    cerr << "FEL: Filen gick inte att öppna!" << endl;
+    goto file;
+  }
 
-	string line;
-	vector<Racer> data;
+  string line;
+  vector<Racer> data;
 
-	while (getline(file, line)) {
-			Racer racer;
-			vector<string> tokens = split(line);
-			racer.name = tokens[0];
-			racer.hours = stoi(tokens[1]);
-			racer.minutes = stoi(tokens[2]);
-			racer.seconds = stoi(tokens[3]);
-			data.push_back(racer);
-	}
-	file.close();
+  while (getline(file, line)) {
+    Racer racer;
+    vector<string> tokens = split(line);
+    racer.name = tokens[0];
+    racer.hours = stoi(tokens[1]);
+    racer.minutes = stoi(tokens[2]);
+    racer.seconds = stoi(tokens[3]);
+    data.push_back(racer);
+  }
+  file.close();
 
 rows:
-	int rows;
-	cout << "Antal rader: ";
+  int rows;
+  cout << "Antal rader: ";
 
-	while(!(cin >> rows)) {
-		cerr << "FEL: Inmatningen måste vara ett heltal!" << endl;
-		cin.clear();
+  while (!(cin >> rows)) {
+    cerr << "FEL: Inmatningen måste vara ett heltal!" << endl;
+    cin.clear();
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		goto rows;
-	}
+    goto rows;
+  }
 
-	if(data.size() < size_t(rows)) {
-		cerr << "FEL: Det finns inte " << rows << " rader i filen!" << endl;
-		cin.clear();
-		goto rows;
-	}
+  if (data.size() < size_t(rows)) {
+    cerr << "FEL: Det finns inte " << rows << " rader i filen!" << endl;
+    cin.clear();
+    goto rows;
+  }
 
-	sort(data.begin(), data.end(), 
-		[](Racer a, Racer b){
-			int a_total = a.hours * 60 * 60 + a.minutes * 60 + a.seconds;
-			int b_total = b.hours * 60 * 60 + b.minutes * 60 + b.seconds;
-			return a_total < b_total;
-		});
+  sort(data.begin(), data.end(), [](Racer a, Racer b) {
+    int a_total = a.hours * 60 * 60 + a.minutes * 60 + a.seconds;
+    int b_total = b.hours * 60 * 60 + b.minutes * 60 + b.seconds;
+    return a_total < b_total;
+  });
 
-	printCentered("Namn", 10);
-	cout << "|";
-	printCentered("Tid", 10);
-	cout << "\n" << setfill('=') << setw(20) << "" << "\n";
-	for (size_t i = 0; i < size_t(rows) && i < data.size(); ++i) {
-		Racer& racer = data[i];
-		cout << setfill(' ') << setw(9) << right << racer.name << " ";
-		cout << "|";
-		cout << setw(2) << racer.hours << ":"
-			 << setfill('0') << setw(2) << racer.minutes << ":"
-			 << setfill('0') << setw(2) << racer.seconds;
-		cout << "\n";
-	}
+  printCentered("Namn", 10);
+  cout << "|";
+  printCentered("Tid", 10);
+  cout << "\n" << setfill('=') << setw(20) << "" << "\n";
+  for (size_t i = 0; i < size_t(rows) && i < data.size(); ++i) {
+    Racer &racer = data[i];
+    cout << setfill(' ') << setw(9) << right << racer.name << " ";
+    cout << "|";
+    cout << setw(2) << racer.hours << ":" << setfill('0') << setw(2)
+         << racer.minutes << ":" << setfill('0') << setw(2) << racer.seconds;
+    cout << "\n";
+  }
 }
